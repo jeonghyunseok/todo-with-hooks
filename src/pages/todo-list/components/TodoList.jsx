20b69b48cf6def96509914628561233709a3a1e1
ByungDae Sohn <miconblog@gmail.com>
2019-04-16 22:44:12 +0900

    [실습] TodoList 컴포넌트는 useState 훅을 이용해 함수형 컴포넌트로 변경하기

--- src/pages/todo-list/components/TodoList.jsx
+++ src/pages/todo-list/components/TodoList.jsx
@@ -4,80 +4,82 @@ import StyledTodoList from './TodoList.style';
 import { Input, Button, Icon } from 'antd';
 import PropTypes from 'prop-types';
 
-class TodoList extends React.Component {
-  static propTypes = {
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string,
-        completed: PropTypes.bool,
-      })
-    ).isRequired,
-    onUnselectAll: PropTypes.func,
-  };
+function TodoList(props) {
+  const {
+    items,
+    onCreate,
+    onDelete,
+    onEdit,
+    onChangeComplete,
+    onUnselectAll,
+    onSelectAll,
+  } = props;
+  const isSelectedAll = items.every(({ completed }) => completed);
 
-  constructor(props) {
-    super(props);
+  const inputRef = React.createRef();
 
-    this.input = React.createRef();
-  }
-
-  handleEnterTodoText = e => {
-    this.props.onCreate(e.target.value);
-    this.input.current.setState({
+  const handleEnterTodoText = e => {
+    onCreate(e.target.value);
+    inputRef.current.setState({
       value: '',
     });
   };
 
-  handleChangeComplteItem = (item, index, value) => {
+  const handleChangeComplteItem = (item, index, value) => {
     item.completed = value;
-    this.props.onChangeComplete(index, item);
+    onChangeComplete(index, item);
   };
 
-  render() {
-    const { items } = this.props;
-    const isSelectedAll = items.every(({ completed }) => completed);
+  return (
+    <StyledTodoList className="TodoList">
+      <div>
+        <Input
+          ref={inputRef}
+          onPressEnter={handleEnterTodoText}
+          addonAfter={<Icon type="plus" />}
+        />
+      </div>
 
-    return (
-      <StyledTodoList className="TodoList">
+      <div>
+        {items.map((item, index) => {
+          return (
+            <TodoListItem
+              key={`item-${index}`}
+              {...item}
+              onDelete={e => onDelete(index)}
+              onEdit={value => onEdit(index, value)}
+              onChangeSelect={e =>
+                handleChangeComplteItem(item, index, e.target.checked)
+              }
+            />
+          );
+        })}
+      </div>
+
+      <div className="footer">
+        {isSelectedAll ? (
+          <Button onClick={onUnselectAll}>선택해제</Button>
+        ) : (
+          <Button onClick={onSelectAll}>전체선택</Button>
+        )}
+
         <div>
-          <Input
-            ref={this.input}
-            onPressEnter={this.handleEnterTodoText}
-            addonAfter={<Icon type="plus" />}
-          />
+          <span>할일 {items.filter(item => !item.completed).length}</span>
+          <span>완료 {items.filter(item => item.completed).length}</span>
         </div>
-
-        <div>
-          {items.map((item, index) => {
-            return (
-              <TodoListItem
-                key={`item-${index}`}
-                {...item}
-                onDelete={e => this.props.onDelete(index)}
-                onEdit={value => this.props.onEdit(index, value)}
-                onChangeSelect={e =>
-                  this.handleChangeComplteItem(item, index, e.target.checked)
-                }
-              />
-            );
-          })}
-        </div>
-
-        <div className={'footer'}>
-          {isSelectedAll ? (
-            <Button onClick={this.props.onUnselectAll}>선택해제</Button>
-          ) : (
-            <Button onClick={this.props.onSelectAll}>전체선택</Button>
-          )}
-
-          <div>
-            <span>할일 {items.filter(item => !item.completed).length}</span>
-            <span>완료 {items.filter(item => item.completed).length}</span>
-          </div>
-        </div>
-      </StyledTodoList>
-    );
-  }
+      </div>
+    </StyledTodoList>
+  );
 }
+
+TodoList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      completed: PropTypes.bool,
+    })
+  ).isRequired,
+  onUnselectAll: PropTypes.func,
+};
 
 export default TodoList;
